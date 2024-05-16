3084a683eda4a42031f84832bf9a4887da1b3145
paulo_sneto <paulo_sneto@yahoo.com>
2024-05-16 16:47:35 -0300

    feat: create PagamentoErroProducerService worker-api

---
+++ worker-api/src/main/java/com/api/worker/service/PagamentoErroProducerService.java
@@ -0,0 +1,20 @@
+package com.api.worker.service;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagamentoErroProducerService {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    public void gerarResposta(String mensagem){
+        amqpTemplate.convertAndSend(
+                "pagamento-response-erro-exchange",
+                "pagamento-response-erro-rout-key",
+                mensagem);
+    }
+
+}
